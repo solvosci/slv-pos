c0139ec2dca278386929a5765a21e3fb515bae2a
David Alonso (Solvos) <david.alonso@solvos.es>
2020-01-27 12:49:22 +0100

    [ADD] pos_report_saledetails_taxes_by_ticket

---
+++ pos_report_saledetails_taxes_by_ticket/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Taxes detailed by ticket in Sale Details report",
+    "summary":
+        "Adds a taxes detailed by ticket section in Sale Details report",
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "12.0.1.0.0",
+    "category": "Point Of Sale",
+    "website": "https://github.com/solvosci/slv-pos",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/report_saledetails.xml",
+    ],
+    'installable': True,
+}
---
+++ pos_report_saledetails_taxes_by_ticket/report/report_saledetails.py
@@ -0,0 +1,74 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+from odoo import api, models, _
+
+
+class ReportSaleDetails(models.AbstractModel):
+    _inherit = 'report.point_of_sale.report_saledetails'
+
+    @api.model
+    def _prepare_orders(
+            self, date_start=False, date_stop=False, configs=False):
+        # TODO make PR to Odoo with this hook method
+        return self.env['pos.order'].search([
+            ('date_order', '>=', date_start),
+            ('date_order', '<=', date_stop),
+            ('state', 'in', ['paid', 'invoiced', 'done']),
+            ('config_id', 'in', configs.ids)])
+
+    @api.model
+    def get_sale_details(
+            self, date_start=False, date_stop=False, configs=False):
+        orders = self._prepare_orders(
+            date_start=date_start, date_stop=date_stop, configs=configs)
+
+        taxes_by_ticket = []
+        for order in orders:
+            currency = order.session_id.currency_id
+            for line in order.lines:
+                if line.tax_ids_after_fiscal_position:
+                    line_taxes = \
+                        line.tax_ids_after_fiscal_position.compute_all(
+                            line.price_unit *
+                            (1 - (line.discount or 0.0) / 100.0),
+                            currency, line.qty, product=line.product_id,
+                            partner=line.order_id.partner_id or False)
+                    for tax in line_taxes['taxes']:
+                        tax_detail = next(
+                            (n for n in taxes_by_ticket
+                             if n['id'] == tax['id']
+                             and n['ticket'] == order.pos_reference), None)
+                        if tax_detail:
+                            tax_detail['tax_amount'] += tax['amount']
+                            tax_detail['base_amount'] += tax['base']
+                        else:
+                            taxes_by_ticket.append({
+                                'id': tax['id'],
+                                'ticket': order.pos_reference,
+                                'name': tax['name'],
+                                'tax_amount': tax['amount'],
+                                'base_amount': tax['base']})
+                else:
+                    no_tax_detail = next(
+                        (n for n in taxes_by_ticket
+                         if n['id'] == 0
+                         and n['ticket'] == order.pos_reference), None)
+                    if no_tax_detail:
+                        no_tax_detail['base_amount'] += \
+                            line.price_subtotal_incl
+                    else:
+                        taxes_by_ticket.append({
+                            'id': 0,
+                            'ticket': order.pos_reference,
+                            'name': _('No Taxes'),
+                            'tax_amount': 0.0,
+                            'base_amount': line.price_subtotal_incl})
+
+        taxes_by_ticket.sort(key=lambda x: x['ticket'])
+
+        sale_details = super().get_sale_details(
+            date_start=date_start, date_stop=date_stop, configs=configs)
+        sale_details['taxes_by_ticket'] = taxes_by_ticket
+
+        return sale_details
